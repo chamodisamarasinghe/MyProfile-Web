043d7a07a5aa5e195ffffb93b8477cdb679fb027
chamodisamarasinghe <chamodisandunika98@gmail.com>
2022-03-11 10:46:59 +0530

    Added item model

---
+++ My Projects/SPA/model/ItemDTO.js
@@ -0,0 +1,47 @@
+function ItemDTO(icode,iname,iprice,iqty) {
+
+    var code = icode;
+    var name = iname;
+    var price = iprice;
+    var qty = iqty;
+
+
+    Object.defineProperty(this, "icode", {
+        get: function () {
+            return code;
+        },
+        set: function (icode) {
+            this.code = icode;
+        }
+    });
+
+
+    Object.defineProperty(this, "iname", {
+        get: function () {
+            return name;
+        },
+        set: function (iname) {
+            this.name = iname;
+        }
+    });
+
+
+    Object.defineProperty(this, "iprice", {
+        get: function () {
+            return price;
+        },
+        set: function (iprice) {
+            this.price = iprice;
+        }
+    });
+
+
+    Object.defineProperty(this, "iqty", {
+        get: function () {
+            return qty;
+        },
+        set: function (iqty) {
+            this.qty = iqty;
+        }
+    });
+}
